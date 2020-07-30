f935b4ab49eb04eb242f57e3591a2e3ae5ac4864
devansh071999 <devanshsampatjain@gmail.com>
2020-07-30 19:18:50 +0530

    footer added

---
+++ frontend/src/components/parts/footer.js
@@ -0,0 +1,23 @@
+import react from "react"
+
+const footer=()=>{
+    return(
+        <footer>
+        <div className="container">
+
+            <h1>Mitte</h1>
+            <p className="copyright-line">© Copyright 2019 - All rights reserved.</p>
+            <ul className="social-list">
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+
+        </div>
+
+    </footer>
+    )
+}
+
+export default footer;
