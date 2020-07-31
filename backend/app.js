164ac3517c659e59b72bc0e638902df07adaace4
devansh071999 <devanshsampatjain@gmail.com>
2020-07-31 19:16:06 +0530

    few things modified

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,36 @@
 const express=require('express')
+const mongoose=require('mongoose')
+const {MONGOURI}=require('./keys')
 const app=express()
 const PORT=5000
 
+//connection to mongodb
+mongoose.connect(MONGOURI,
+    { useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+
+mongoose.connection.on("connected",()=>{
+    console.log("Connected to mongodb")
+})
+
+mongoose.connection.on("error",(error)=>{
+    console.log("Error: " + error)
+})
+
 app.get("/",(req,res)=>{
     res.send('Hello Folks')
+})
+app.get("/home",(req,res)=>{
+    res.send('Home')
+})
+
+app.get("/category",(req,res)=>{
+    res.send('Category')
+})
+
+app.get("/single",(req,res)=>{
+    res.send('Single')
 })
 
 app.listen(PORT,()=>{
---
+++ backend/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI:"mongodb+srv://Devansh:travelblog@cluster0.p1rnc.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}
